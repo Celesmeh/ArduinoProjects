e5795bbfd5477395d4c6a3f4425ede43a4736806
Celesmeh <60585813+Celesmeh@users.noreply.github.com>
2020-08-20 00:01:42 -0400

    Update Hydrator.ino

--- Hydrator/Hydrator.ino
+++ Hydrator/Hydrator.ino
@@ -1,35 +1,42 @@
+43eAR
 
-int r = 0, g = 0, b = 255, count = 0;
+7
+int r = 0, g = 255, b = 255, count = 0;
 
 void setup()
 {
   // Debug console
   Serial.begin(9600);
-
-  //Blynk.begin(auth, ssid, pass);
   pinMode(4, INPUT);
   pinMode(2, INPUT);
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(9, OUTPUT);
-
-
 }
 
 void loop()
 {
-  if (digitalRead(4))
-  {
-    setColor(255, 0, 0); // red
-    Serial.println("The red led is on now");
-  }
-
-  else
-  {
-    setColor(00, 255, 255); // red
+  if ( digitalRead(4)) {
+    count = 0;
+    g = 255;
+    b = 255;
+    setColor(0, 255, 255); // red
     Serial.println("The blue led is on now");
   }
-  delay(100);
+  else {
+    for (int i = 0; i <= 60; i++) {
+      if (count < 255) {
+        count++;
+        g--;
+        b--;
+        setColor(count, g, b); // blue
+        delay(12000);
+      }
+//      else {
+//        setColor(255 , 0 , 0);
+//      }
+    }
+  }
 }
 
 void setColor(int red, int green, int blue)
